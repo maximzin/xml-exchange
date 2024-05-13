2cd8203287eca46abc7cabcbe4d2dc46e13866a2
maxim zinovev <maximzin@mail.ru>
2024-05-13 22:05:46 +0500

    first remote commit

---
+++ src/main/java/ru/work/xmlexchange/XmlExchangeApplication.java
@@ -0,0 +1,13 @@
+package ru.work.xmlexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlExchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmlExchangeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/work/xmlexchange/XmlExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.work.xmlexchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmlExchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
