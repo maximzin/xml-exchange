6517ef4ec5e021f375958fa316c343f1771eafac
maxim zinovev <maximzin@mail.ru>
2024-05-17 11:51:54 +0500

    WorkPlace 17 May (working with XML)

--- src/main/java/ru/work/xmlexchange/service/XmlGetFromDb.java
+++ src/main/java/ru/work/xmlexchange/service/XmlGetFromDb.java
@@ -11,10 +11,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class XmlGetFromDb {
@@ -28,8 +25,11 @@ public class XmlGetFromDb {
     }
 
     public List<String> getXml() {
+
         int countXml;
+        String idXml;
         String xml;
+
         List<String> xmlList = new ArrayList<>();
         try {
             countXml = jdbcTemplate.queryForObject("SELECT COUNT(*) FROM tXml", Integer.class);
@@ -39,18 +39,23 @@ public class XmlGetFromDb {
 
         if (countXml != 0) {
             try {
+                //Идёт перебор всех строк в таблице tXml, подразумевается, что значения id идут начиная с 1
                 for (int i = 1; i <= countXml; i++) {
-                    xml = jdbcTemplate.queryForObject(String.format("SELECT xml FROM tXml WHERE id = (%s)", i), String.class);
+                    idXml = jdbcTemplate.queryForObject("SELECT TOP 1 id FROM tXml ORDER BY createDate", String.class);
+                    xml = jdbcTemplate.queryForObject("SELECT TOP 1 xml FROM tXml ORDER BY createDate", String.class);
                     xmlList.add(xml);
                     try {
                         assert xml != null;
-                        saveXmlOnDisk(xml, i);
+
+                        //Опционально: сохранять ли на жесткий диск XML файлы
+                        saveXmlOnDisk(idXml, xml);
+
+                        //Опционально: переносить в tXml_sended и удалять из основной базы tXml
+                        transferXml(idXml, xml);
                     }
                     catch (IOException e) {
                         throw new IOException(e);
                     }
-
-
                 }
             } catch (DataAccessException | IOException e) {
                 throw new RuntimeException(e);
@@ -59,21 +64,26 @@ public class XmlGetFromDb {
         return xmlList;
     }
 
-    private void saveXmlOnDisk(String xml, int count) throws IOException {
+    private void saveXmlOnDisk(String idXml, String xml) throws IOException {
         Calendar calendar = Calendar.getInstance();
         String date = new SimpleDateFormat("dd-MM-yyyy").format(calendar.getTime());
 
 
-        File newDir = new File(String.format("D:\\Работа\\xml\\%s", date));
+        File newDir = new File(String.format("D:\\ШОД\\xml\\%s", date));
         if (!newDir.exists()){
             newDir.mkdirs();
         }
 
-        String path = String.format("D:\\Работа\\xml\\%s\\xml%s.xml", date, count);
+        String path = String.format("D:\\ШОД\\xml\\%s\\%s.xml", date, idXml);
 
         File file = new File(path);
         try (FileOutputStream fos = new FileOutputStream(file)) {
             fos.write(xml.getBytes());
         }
+    }
+
+    private void transferXml(String idXml, String xml) {
+        jdbcTemplate.execute("INSERT INTO tXml_sended (id_xml, xml) SELECT TOP 1 id, xml FROM tXml ORDER BY createDate ");
+        jdbcTemplate.execute("DELETE FROM tXml WHERE id IN (SELECT TOP 1 id FROM tXml ORDER BY createDate)");
     }
 }
